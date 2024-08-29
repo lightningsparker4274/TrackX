4546effa61f43eaae0a3a1ed97a18df12dce41a3
Akash D <akashdurai412@gmail.com>
2024-08-29 11:12:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,88 @@
- 
 import { useState, useEffect } from "react";
-//import {useNavigate} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Register from "./Authentication/Register";
+import Login from "./Authentication/Login";
+import MasterPage from "./component/MasterPage";
+import localforage from "localforage";
+import StoredData from "./component/StoredData";
+import EditPage from "./component/EditPage";
+
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check if user is authenticated
+    localforage.getItem("user").then((user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      }
+    });
+  }, []);
+
+  return (
+    <div className=" font-base">
+      <Router>
+        <Routes>
+          <Route
+            path="/register"
+            element={
+              isAuthenticated ? (
+                <Navigate to="/protected" replace />
+              ) : (
+                <Register setIsAuthenticated={setIsAuthenticated} />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              isAuthenticated ? (
+                <Navigate to="/protected" replace />
+              ) : (
+                <Login setIsAuthenticated={setIsAuthenticated} />
+              )
+            }
+          />
+          <Route
+            path="/protected"
+            element={
+              isAuthenticated ? (
+                <MasterPage setIsAuthenticated={setIsAuthenticated} />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? (
+                <Navigate to="/protected" replace />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+
+          <Route path="/store" element={<StoredData />} />
+
+          <Route path="/files" element={<EditPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+ */
+
+
+import { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -73,7 +155,11 @@ function App() {
           <Route
             path="/store"
             element={
-              isAuthenticated ? <StoredData /> : <Navigate to="/login" replace />
+              isAuthenticated ? (
+                <StoredData />
+              ) : (
+                <Navigate to="/login" replace />
+              )
             }
           />
           <Route
@@ -89,4 +175,3 @@ function App() {
 }
 
 export default App;
-
