e732b7ff27210068066f3b585ce0fa2b21ecc046
Akash D <akashdurai412@gmail.com>
2024-08-30 15:25:34 +0530

    Update MasterPage.jsx

--- src/component/MasterPage.jsx
+++ src/component/MasterPage.jsx
@@ -144,7 +144,7 @@ function MasterPage() {
             <textarea
               className="p-1 border border-gray-300 rounded-md focus:ring-2 focus: ring-sky-300"
               rows={4}
-              cols={38}
+              cols={33}
               value={formData.description}
               onChange={handleInputChange}
               name="description"
