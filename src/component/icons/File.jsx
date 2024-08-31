3b4e9159a1197d60b1d2a22dfe52825b68514a2b
Akash D <akashdurai412@gmail.com>
2024-08-31 11:38:17 +0530

    Update File.jsx

--- src/component/icons/File.jsx
+++ src/component/icons/File.jsx
@@ -8,7 +8,7 @@ const File = () => {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="size-6"
+        className="size-6 mt-2"
       >
         <path
           strokeLinecap="round"
